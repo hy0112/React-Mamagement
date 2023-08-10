61ffb810730519ea9d44baac48c0bbef3daa7f3e
hy0112 <66620503+hy0112@users.noreply.github.com>
2023-08-10 18:44:21 +0900

    today

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray- background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer={
+  'name' : '김현영',
+  'birthday' : '940112',
+  'gender' : '남자',
+  'job' : '취준생',
 }
 
+class App extends Component {
+    render(){
+      return (
+        <Customer 
+          name={customer.name}
+          birthday={customer.birthday}
+          gender={customer.gender}
+          job={customer.job}
+        />
+          
+      )
+  }
+} 
 export default App;
